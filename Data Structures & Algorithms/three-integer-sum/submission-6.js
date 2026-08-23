ceec1356e2b8eeb2f7d5a3df8090a20b40d633b4
Om Vyas <ovyas24@gmail.com>
2026-08-23 20:56:26 +0530

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const sarray = nums.sort((a, b) => a - b);
+        let result = [];
+        for (let i = 0; i < sarray.length; i++) {
+            let target = -1 * sarray[i];
+            let left = i + 1;
+            let right = sarray.length - 1;
+            if (i>0 && sarray[i] == sarray[i - 1]) {
+                continue;
+            }
+            // if (sarray[i] !== sarray[i - 1]) {
+            while (left < right) {
+                let sum = sarray[left] + sarray[right];
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    result.push([sarray[i], sarray[left], sarray[right]]);
+                    left++;
+                    right--;
+                    while (left < right && sarray[left] == sarray[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && sarray[right] == sarray[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+            // }
+        }
+        return result;
+    }
+}
